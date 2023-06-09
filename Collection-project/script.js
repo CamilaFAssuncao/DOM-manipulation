faa05e91adf456c58091c65886207a85b28d2dad
Camila Fernandes Assunção <129509305+CamilaFAssuncao@users.noreply.github.com>
2023-06-09 12:30:14 +0200

    html skeleton, pics and js collection

---
+++ Collection-project/script.js
@@ -0,0 +1,91 @@
+const collection = [
+{
+    name: 'Birds of Prey',
+    director: 'Cathy Yan',
+    releaseYear: 2020,
+    picture: 'link/to/a/picture',
+    genre: ['Action', 'Comedy', 'Crime'],
+    cast: ['Margot Robbie', 'Rosie Perez', 'Mary Elizabeth Winstead']
+},
+
+{
+    name: 'Clueless',
+    director: 'Amy Heckerling',
+    releaseYear: 1995,
+    picture: 'link/to/a/picture',
+    genre: ['Comedy', 'Romance'],
+    cast: ['Alicia Silverstone', 'Stacey Dash', 'Brittany Murphy']
+},
+
+{
+    name: 'Lady Bird',
+    director: 'Greta Gerwig',
+    releaseYear: 2017,
+    picture: 'link/to/a/picture',
+    genre: ['Comedy', 'Drama'],
+    cast: ['Saoirse Ronan', 'Laurie Metcalf', 'Timothée Chalamet']
+},
+
+{
+    name: 'Little Women',
+    director: 'Greta Gerwig',
+    releaseYear: 2019,
+    picture: 'link/to/a/picture',
+    genre: ['Drama', 'Romance'],
+    cast: ['Saoirse Ronan', 'Emma Watson', 'Florence Pugh', 'Timothée Chalamet']
+},
+
+{
+    name: 'Selma',
+    director: 'Ava DuVernay',
+    releaseYear: 2014,
+    picture: 'link/to/a/picture',
+    genre: ['Biography', 'Drama', 'History'],
+    cast: ['David Oyelowo', 'Carmen Ejogo', 'Jim France', 'Oprah Winfrey']
+},
+
+{
+    name: 'The Woman King',
+    director: 'Gina Prince-Bythewood',
+    releaseYear: 2022,
+    picture: 'link/to/a/picture',
+    genre: ['Action', 'Drama', 'History'],
+    cast: ['Viola Davis', 'Thuso Mbedu', 'Lashana Lynch']
+},
+
+{
+    name: 'Fleabag',
+    director: 'Phoebe Waller-Bridge',
+    releaseYear: 2016,
+    picture: 'link/to/a/picture',
+    genre: ['Comedy', 'Drama'],
+    cast: ['Phoebe Waller-Bridge', 'Sian Clifford', 'Olivia Colman', 'Jenny Rainsford']
+},
+
+{
+    name: 'The Handmaids Tale',
+    director: ['Bruce Miller', ''],
+    releaseYear: 2017,
+    picture: 'link/to/a/picture',
+    genre: ['Drama', 'Sci-Fi', 'Thriller'],
+    cast: ['Elisabeth Moss', 'Yvonne Strahovski', 'Ann Dowd', 'O-T Fagbenle']
+},
+
+{
+    name: 'Orange Is The New Black',
+    director: 'Jenji Kohan',
+    releaseYear: 2013,
+    picture: 'link/to/a/picture',
+    genre: ['Comedy', 'Crime', 'Drama'],
+    cast: ['Taylor Schilling', 'Kate Mulgrew', 'Uzo Aduba', 'Danielle Brooks']
+},
+
+{
+    name: 'The Crown',
+    director: ['Benjamin Caron', 'Jessica Hobbs'],
+    releaseYear: 2016,
+    picture: 'link/to/a/picture',
+    genre: ['Biography', 'Drama', 'History'],
+    cast: ['Claire Foy', 'Olivia Colman', 'Imelda Staunton', 'Helena Bonham Carter']
+}
+]
